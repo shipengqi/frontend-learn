d29e033661d36c6930cd3f0586c00e545cb0448c
shipengqi <pooky.shipengqi@gmail.com>
2018-09-12 18:39:19 +0800

    Add more notes

--- vue_src/core/instance/init.js
+++ vue_src/core/instance/init.js
@@ -1,34 +1,55 @@
 /* @flow */
 
 import config from '../config'
-import { initProxy } from './proxy'
-import { initState } from './state'
-import { initRender } from './render'
-import { initEvents } from './events'
-import { mark, measure } from '../util/perf'
-import { initLifecycle, callHook } from './lifecycle'
-import { initProvide, initInjections } from './inject'
-import { extend, mergeOptions, formatComponentName } from '../util/index'
+import {
+  initProxy
+} from './proxy'
+import {
+  initState
+} from './state'
+import {
+  initRender
+} from './render'
+import {
+  initEvents
+} from './events'
+import {
+  mark,
+  measure
+} from '../util/perf'
+import {
+  initLifecycle,
+  callHook
+} from './lifecycle'
+import {
+  initProvide,
+  initInjections
+} from './inject'
+import {
+  extend,
+  mergeOptions,
+  formatComponentName
+} from '../util/index'
 
 let uid = 0
 
 /*initMixin，在Vue的原型上增加 _init 方法， 用来初始化 Vue 实例*/
-export function initMixin (Vue: Class<Component>) {
-  Vue.prototype._init = function (options?: Object) {
+export function initMixin(Vue: Class < Component > ) {
+  Vue.prototype._init = function(options ? : Object) {
     const vm: Component = this
-    // a uid
+      // a uid
     vm._uid = uid++
 
-    // 在非生产环境下，config.performance 和 mark 都为真，执行下面的代码，
-    // config.performance 来自于 core/config.js 文件，Vue.config 同样引用了这个对象
-    // Vue.config.performance 设置为 true，可开启性能追踪，追踪下面四个场景：
-    // 1、组件初始化(component init)
-    // 2、编译(compile)，将模板(template)编译成渲染函数
-    // 3、渲染(render)，其实就是渲染函数的性能，或者说渲染函数执行且生成虚拟DOM(vnode)的性能
-    // 4、打补丁(patch)，将虚拟DOM渲染为真实DOM的性能
-    // 实现的方式就是在初始化的代码的开头和结尾分别使用 mark 函数打上两个标记，然后通过 measure 函数对这两个标记点进行性能计算。
-    let startTag, endTag
-    /* istanbul ignore if */
+      // 在非生产环境下，config.performance 和 mark 都为真，执行下面的代码，
+      // config.performance 来自于 core/config.js 文件，Vue.config 同样引用了这个对象
+      // Vue.config.performance 设置为 true，可开启性能追踪，追踪下面四个场景：
+      // 1、组件初始化(component init)
+      // 2、编译(compile)，将模板(template)编译成渲染函数
+      // 3、渲染(render)，其实就是渲染函数的性能，或者说渲染函数执行且生成虚拟DOM(vnode)的性能
+      // 4、打补丁(patch)，将虚拟DOM渲染为真实DOM的性能
+      // 实现的方式就是在初始化的代码的开头和结尾分别使用 mark 函数打上两个标记，然后通过 measure 函数对这两个标记点进行性能计算。
+      let startTag, endTag
+        /* istanbul ignore if */
     if (process.env.NODE_ENV !== 'production' && config.performance && mark) {
       startTag = `vue-perf-start:${vm._uid}`
       endTag = `vue-perf-end:${vm._uid}`
@@ -38,8 +59,8 @@ export function initMixin (Vue: Class<Component>) {
     // _isVue 设置为 true。用来标识对象是一个 Vue 实例，避免该对象被响应系统观测
     // a flag to avoid this being observed
     vm._isVue = true
-    // _isComponent 是一个内部选项，在 Vue 创建组件的时候才会有
-    // merge options
+      // _isComponent 是一个内部选项，在 Vue 创建组件的时候才会有
+      // merge options
     if (options && options._isComponent) {
       // optimize internal component instantiation
       // since dynamic options merging is pretty slow, and none of the
@@ -48,9 +69,9 @@ export function initMixin (Vue: Class<Component>) {
     } else {
       // 在 Vue 实例上添加 $options 属性，这个属性用于当前 Vue 的初始化
       vm.$options = mergeOptions(
-        resolveConstructorOptions(vm.constructor),
-        options || {},
-        vm
+        resolveConstructorOptions(vm.constructor), // What? 进入 core/instance/init.js 这个函数最后返回的是 Vue.options
+        options || {}, //调用 new Vue() 实例时，传入的对象
+        vm // 当前Vue实例
       )
     }
     /* istanbul ignore else */
@@ -85,9 +106,9 @@ export function initMixin (Vue: Class<Component>) {
   }
 }
 
-export function initInternalComponent (vm: Component, options: InternalComponentOptions) {
+export function initInternalComponent(vm: Component, options: InternalComponentOptions) {
   const opts = vm.$options = Object.create(vm.constructor.options)
-  // doing this because it's faster than dynamic enumeration.
+    // doing this because it's faster than dynamic enumeration.
   const parentVnode = options._parentVnode
   opts.parent = options.parent
   opts._parentVnode = parentVnode
@@ -104,18 +125,20 @@ export function initInternalComponent (vm: Component, options: InternalComponent
   }
 }
 
-export function resolveConstructorOptions (Ctor: Class<Component>) {
+// 作用是解析构造者的 options
+export function resolveConstructorOptions(Ctor: Class < Component > ) {
+  // Ctor 就是 vm.constructor
   let options = Ctor.options
-  if (Ctor.super) {
+  if (Ctor.super) { // super 属性与 Vue.extend 有关
     const superOptions = resolveConstructorOptions(Ctor.super)
     const cachedSuperOptions = Ctor.superOptions
     if (superOptions !== cachedSuperOptions) {
       // super option changed,
       // need to resolve new options.
       Ctor.superOptions = superOptions
-      // check if there are any late-modified/attached options (#4976)
+        // check if there are any late-modified/attached options (#4976)
       const modifiedOptions = resolveModifiedOptions(Ctor)
-      // update base extend options
+        // update base extend options
       if (modifiedOptions) {
         extend(Ctor.extendOptions, modifiedOptions)
       }
@@ -128,7 +151,7 @@ export function resolveConstructorOptions (Ctor: Class<Component>) {
   return options
 }
 
-function resolveModifiedOptions (Ctor: Class<Component>): ?Object {
+function resolveModifiedOptions(Ctor: Class < Component > ): ? Object {
   let modified
   const latest = Ctor.options
   const extended = Ctor.extendOptions
@@ -142,7 +165,7 @@ function resolveModifiedOptions (Ctor: Class<Component>): ?Object {
   return modified
 }
 
-function dedupe (latest, extended, sealed) {
+function dedupe(latest, extended, sealed) {
   // compare latest and sealed to ensure lifecycle hooks won't be duplicated
   // between merges
   if (Array.isArray(latest)) {
--- vue_src/core/util/options.js
+++ vue_src/core/util/options.js
@@ -1,9 +1,14 @@
 /* @flow */
 
 import config from '../config'
-import { warn } from './debug'
-import { nativeWatch } from './env'
-import { set } from '../observer/index'
+import {
+  warn
+} from './debug'
+import {
+  nativeWatch
+} from './env'
+import {set
+} from '../observer/index'
 
 import {
   ASSET_TYPES,
@@ -25,13 +30,17 @@ import {
  * how to merge a parent option value and a child option
  * value into the final value.
  */
+// 这时的 strats 合并策略还是一个空对象
 const strats = config.optionMergeStrategies
 
 /**
  * Options with restrictions
  */
+// 添加 el、propsData 的合并策略
+// 这两个策略先判断了是否有 vm 参数，没有就打印警告，提示el、propsData只能使用 new 创建实例时使用
+// 比如在子组件中使用了 el 选项，就会得到警告
 if (process.env.NODE_ENV !== 'production') {
-  strats.el = strats.propsData = function (parent, child, vm, key) {
+  strats.el = strats.propsData = function(parent, child, vm, key) {
     if (!vm) {
       warn(
         `option "${key}" can only be used during instance ` +
@@ -45,7 +54,7 @@ if (process.env.NODE_ENV !== 'production') {
 /**
  * Helper that recursively merges two data objects together.
  */
-function mergeData (to: Object, from: ?Object): Object {
+function mergeData(to: Object, from: ? Object): Object {
   if (!from) return to
   let key, toVal, fromVal
   const keys = Object.keys(from)
@@ -65,11 +74,11 @@ function mergeData (to: Object, from: ?Object): Object {
 /**
  * Data
  */
-export function mergeDataOrFn (
+export function mergeDataOrFn(
   parentVal: any,
   childVal: any,
-  vm?: Component
-): ?Function {
+  vm ? : Component
+): ? Function {
   if (!vm) {
     // in a Vue.extend merge, both should be functions
     if (!childVal) {
@@ -83,21 +92,21 @@ export function mergeDataOrFn (
     // merged result of both functions... no need to
     // check if parentVal is a function here because
     // it has to be a function to pass previous merges.
-    return function mergedDataFn () {
+    return function mergedDataFn() {
       return mergeData(
         typeof childVal === 'function' ? childVal.call(this, this) : childVal,
         typeof parentVal === 'function' ? parentVal.call(this, this) : parentVal
       )
     }
   } else {
-    return function mergedInstanceDataFn () {
+    return function mergedInstanceDataFn() {
       // instance merge
-      const instanceData = typeof childVal === 'function'
-        ? childVal.call(vm, vm)
-        : childVal
-      const defaultData = typeof parentVal === 'function'
-        ? parentVal.call(vm, vm)
-        : parentVal
+      const instanceData = typeof childVal === 'function' ?
+        childVal.call(vm, vm) :
+        childVal
+      const defaultData = typeof parentVal === 'function' ?
+        parentVal.call(vm, vm) :
+        parentVal
       if (instanceData) {
         return mergeData(instanceData, defaultData)
       } else {
@@ -107,11 +116,11 @@ export function mergeDataOrFn (
   }
 }
 
-strats.data = function (
+strats.data = function(
   parentVal: any,
   childVal: any,
-  vm?: Component
-): ?Function {
+  vm ? : Component
+): ? Function {
   if (!vm) {
     if (childVal && typeof childVal !== 'function') {
       process.env.NODE_ENV !== 'production' && warn(
@@ -132,17 +141,15 @@ strats.data = function (
 /**
  * Hooks and props are merged as arrays.
  */
-function mergeHook (
-  parentVal: ?Array<Function>,
-  childVal: ?Function | ?Array<Function>
-): ?Array<Function> {
-  return childVal
-    ? parentVal
-      ? parentVal.concat(childVal)
-      : Array.isArray(childVal)
-        ? childVal
-        : [childVal]
-    : parentVal
+function mergeHook(
+  parentVal: ? Array < Function > ,
+  childVal : ? Function | ? Array < Function >
+) : ? Array < Function > {
+  return childVal ?
+    parentVal ?
+    parentVal.concat(childVal) :
+    Array.isArray(childVal) ?
+    childVal : [childVal] : parentVal
 }
 
 LIFECYCLE_HOOKS.forEach(hook => {
@@ -156,12 +163,12 @@ LIFECYCLE_HOOKS.forEach(hook => {
  * a three-way merge between constructor options, instance
  * options and parent options.
  */
-function mergeAssets (
-  parentVal: ?Object,
-  childVal: ?Object,
-  vm?: Component,
-  key: string
-): Object {
+function mergeAssets(
+  parentVal: ? Object,
+  childVal : ? Object,
+  vm ? : Component,
+  key : string
+) : Object {
   const res = Object.create(parentVal || null)
   if (childVal) {
     process.env.NODE_ENV !== 'production' && assertObjectType(key, childVal, vm)
@@ -171,7 +178,7 @@ function mergeAssets (
   }
 }
 
-ASSET_TYPES.forEach(function (type) {
+ASSET_TYPES.forEach(function(type) {
   strats[type + 's'] = mergeAssets
 })
 
@@ -181,16 +188,16 @@ ASSET_TYPES.forEach(function (type) {
  * Watchers hashes should not overwrite one
  * another, so we merge them as arrays.
  */
-strats.watch = function (
-  parentVal: ?Object,
-  childVal: ?Object,
-  vm?: Component,
-  key: string
-): ?Object {
+strats.watch = function(
+  parentVal: ? Object,
+  childVal : ? Object,
+  vm ? : Component,
+  key : string
+): ? Object {
   // work around Firefox's Object.prototype.watch...
   if (parentVal === nativeWatch) parentVal = undefined
   if (childVal === nativeWatch) childVal = undefined
-  /* istanbul ignore if */
+    /* istanbul ignore if */
   if (!childVal) return Object.create(parentVal || null)
   if (process.env.NODE_ENV !== 'production') {
     assertObjectType(key, childVal, vm)
@@ -204,9 +211,9 @@ strats.watch = function (
     if (parent && !Array.isArray(parent)) {
       parent = [parent]
     }
-    ret[key] = parent
-      ? parent.concat(child)
-      : Array.isArray(child) ? child : [child]
+    ret[key] = parent ?
+      parent.concat(child) :
+      Array.isArray(child) ? child : [child]
   }
   return ret
 }
@@ -215,44 +222,46 @@ strats.watch = function (
  * Other object hashes.
  */
 strats.props =
-strats.methods =
-strats.inject =
-strats.computed = function (
-  parentVal: ?Object,
-  childVal: ?Object,
-  vm?: Component,
-  key: string
-): ?Object {
-  if (childVal && process.env.NODE_ENV !== 'production') {
-    assertObjectType(key, childVal, vm)
+  strats.methods =
+  strats.inject =
+  strats.computed = function(
+    parentVal: ? Object,
+    childVal : ? Object,
+    vm ? : Component,
+    key : string
+  ): ? Object {
+    if (childVal && process.env.NODE_ENV !== 'production') {
+      assertObjectType(key, childVal, vm)
+    }
+    if (!parentVal) return childVal
+    const ret = Object.create(null)
+    extend(ret, parentVal)
+    if (childVal) extend(ret, childVal)
+    return ret
   }
-  if (!parentVal) return childVal
-  const ret = Object.create(null)
-  extend(ret, parentVal)
-  if (childVal) extend(ret, childVal)
-  return ret
-}
 strats.provide = mergeDataOrFn
 
 /**
  * Default strategy.
  */
-const defaultStrat = function (parentVal: any, childVal: any): any {
-  return childVal === undefined
-    ? parentVal
-    : childVal
+const defaultStrat = function(parentVal: any, childVal: any): any {
+  return childVal === undefined ?
+    parentVal :
+    childVal
 }
 
 /**
  * Validate component names
  */
-function checkComponents (options: Object) {
+function checkComponents(options: Object) {
+  // 遍历每个组件
   for (const key in options.components) {
     validateComponentName(key)
   }
 }
-
-export function validateComponentName (name: string) {
+// 
+export function validateComponentName(name: string) {
+  // 组件的名字由普通的字符和中横线(-)组成，且必须以字母开头
   if (!/^[a-zA-Z][\w-]*$/.test(name)) {
     warn(
       'Invalid component name: "' + name + '". Component names ' +
@@ -260,6 +269,9 @@ export function validateComponentName (name: string) {
       'and must start with a letter.'
     )
   }
+  // isBuiltInTag 判断组件的名字是否是内置的标签
+  // config.isReservedTag 判断组件的名字是否是保留标签
+  // config.isReservedTag 在 platforms/web/runtime/index.js 中添加的
   if (isBuiltInTag(name) || config.isReservedTag(name)) {
     warn(
       'Do not use built-in or reserved HTML elements as component ' +
@@ -272,29 +284,51 @@ export function validateComponentName (name: string) {
  * Ensure all props option syntax are normalized into the
  * Object-based format.
  */
-function normalizeProps (options: Object, vm: ?Component) {
+// 将 props 规范化为对象 
+function normalizeProps(options: Object, vm: ? Component) {
   const props = options.props
   if (!props) return
   const res = {}
   let i, val, name
-  if (Array.isArray(props)) {
+  if (Array.isArray(props)) { // 数组转为对象
     i = props.length
     while (i--) {
       val = props[i]
       if (typeof val === 'string') {
         name = camelize(val)
-        res[name] = { type: null }
+        res[name] = {
+          type: null
+        }
       } else if (process.env.NODE_ENV !== 'production') {
         warn('props must be strings when using array syntax.')
       }
     }
-  } else if (isPlainObject(props)) {
+  } else if (isPlainObject(props)) { //规范化对象
+    // 如
+    // props: {
+    //   someData1: Number,
+    //   someData2: {
+    //     type: String,
+    //     default: ''
+    //   }
+    // }
+    // 转为
+    // props: {
+    //   someData1: {
+    //     type: Number
+    //   },
+    //   someData2: {
+    //     type: String,
+    //     default: ''
+    //   }
+    // }
     for (const key in props) {
       val = props[key]
       name = camelize(key)
-      res[name] = isPlainObject(val)
-        ? val
-        : { type: val }
+      res[name] = isPlainObject(val) ?
+        val : {
+          type: val
+        }
     }
   } else if (process.env.NODE_ENV !== 'production') {
     warn(
@@ -309,20 +343,28 @@ function normalizeProps (options: Object, vm: ?Component) {
 /**
  * Normalize all injections into Object-based format
  */
-function normalizeInject (options: Object, vm: ?Component) {
+function normalizeInject(options: Object, vm: ? Component) {
+  // 先使用变量 inject 缓存了 options.inject
   const inject = options.inject
+    // 没有 inject 直接返回
   if (!inject) return
+    // normalized 和 options.inject 都引用了同一个空对象
   const normalized = options.inject = {}
   if (Array.isArray(inject)) {
     for (let i = 0; i < inject.length; i++) {
-      normalized[inject[i]] = { from: inject[i] }
+      normalized[inject[i]] = {
+        from: inject[i]
+      }
     }
   } else if (isPlainObject(inject)) {
     for (const key in inject) {
       const val = inject[key]
-      normalized[key] = isPlainObject(val)
-        ? extend({ from: key }, val)
-        : { from: val }
+      normalized[key] = isPlainObject(val) ?
+        extend({
+          from: key
+        }, val) : {
+          from: val
+        }
     }
   } else if (process.env.NODE_ENV !== 'production') {
     warn(
@@ -336,19 +378,22 @@ function normalizeInject (options: Object, vm: ?Component) {
 /**
  * Normalize raw function directives into object format.
  */
-function normalizeDirectives (options: Object) {
+function normalizeDirectives(options: Object) {
   const dirs = options.directives
   if (dirs) {
     for (const key in dirs) {
       const def = dirs[key]
-      if (typeof def === 'function') {
-        dirs[key] = { bind: def, update: def }
+      if (typeof def === 'function') { // 注册的指令是一个函数
+        dirs[key] = {
+          bind: def,
+          update: def
+        }
       }
     }
   }
 }
 
-function assertObjectType (name: string, value: any, vm: ?Component) {
+function assertObjectType(name: string, value: any, vm: ? Component) {
   if (!isPlainObject(value)) {
     warn(
       `Invalid value for option "${name}": expected an Object, ` +
@@ -362,22 +407,35 @@ function assertObjectType (name: string, value: any, vm: ?Component) {
  * Merge two option objects into a new one.
  * Core utility used in both instantiation and inheritance.
  */
-export function mergeOptions (
+// 注意这个函数返回的是一个新的对象，不仅在 _init 函数中用到，在 Vue.extend 中也会用到
+// mergeOptions 函数的作用就是对所有选项的规范化，并且合并选项
+export function mergeOptions(
   parent: Object,
   child: Object,
-  vm?: Component
+  vm ? : Component
 ): Object {
   if (process.env.NODE_ENV !== 'production') {
+    // 校验组件的名字是否符合要求
     checkComponents(child)
   }
-
+  //child 可以是一个对象外，也可以是一个函数
   if (typeof child === 'function') {
-    child = child.options
+    child = child.options // 函数的静态属性 options 作为 child
   }
-
+  // 规范化 props
+  // props 可以是字符串数组，也可以是一个对象，这里要做的就是，不论传入的 props 是什么，都先规范化成一种方式来处理
+  // 在平常开发中可以借鉴这种思路
   normalizeProps(child, vm)
+
+  // 规范化 inject，inject 是2.2.版本新增，配合 provide 使用，可以实现父组件向子组件注入数据
+  // 使用方法与 props 类似
   normalizeInject(child, vm)
+
+  // 规范化 directives，这个选项是用来注册局部指令的
   normalizeDirectives(child)
+
+  // 这里是处理 extends 和 mixins 选项的
+  // 递归调用了 mergeOptions
   const extendsFrom = child.extends
   if (extendsFrom) {
     parent = mergeOptions(parent, extendsFrom, vm)
@@ -387,17 +445,25 @@ export function mergeOptions (
       parent = mergeOptions(parent, child.mixins[i], vm)
     }
   }
+
+  // mergeOptions 返回的是一个新的对象，就是这个 options
+  // 函数的前半段是在规范化选项，接下来就是合并选项
   const options = {}
   let key
   for (key in parent) {
     mergeField(key)
   }
   for (key in child) {
+    // 判断一个属性是否是对象自身的属性
+    // 如果 child 对象的键也在 parent 上出现，那么就不再调用 mergeField，因为在上一个 for in 循环中已经调用过了，避免重复调用。
     if (!hasOwn(parent, key)) {
       mergeField(key)
     }
   }
-  function mergeField (key) {
+
+  function mergeField(key) {
+    // strats 就是在文件头部引用的 config.optionMergeStrategies
+    // 选项覆盖策略是处理如何将父选项值和子选项值合并到最终值的函数。这个对象下包含很多函数，这些函数就可以认为是合并特定选项的策略。
     const strat = strats[key] || defaultStrat
     options[key] = strat(parent[key], child[key], vm, key)
   }
@@ -409,24 +475,24 @@ export function mergeOptions (
  * This function is used because child instances need access
  * to assets defined in its ancestor chain.
  */
-export function resolveAsset (
+export function resolveAsset(
   options: Object,
   type: string,
   id: string,
-  warnMissing?: boolean
+  warnMissing ? : boolean
 ): any {
   /* istanbul ignore if */
   if (typeof id !== 'string') {
     return
   }
   const assets = options[type]
-  // check local registration variations first
+    // check local registration variations first
   if (hasOwn(assets, id)) return assets[id]
   const camelizedId = camelize(id)
   if (hasOwn(assets, camelizedId)) return assets[camelizedId]
   const PascalCaseId = capitalize(camelizedId)
   if (hasOwn(assets, PascalCaseId)) return assets[PascalCaseId]
-  // fallback to prototype chain
+    // fallback to prototype chain
   const res = assets[id] || assets[camelizedId] || assets[PascalCaseId]
   if (process.env.NODE_ENV !== 'production' && warnMissing && !res) {
     warn(
